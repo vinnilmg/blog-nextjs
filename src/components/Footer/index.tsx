ddcd60e2c9070e557292452e58dd5ade41002b4b
vinnilmg <vini.delimagomes@gmail.com>
2024-02-19 19:37:56 -0300

    componente Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,5 @@
+import { Container } from './styles';
+
+export const Footer = () => {
+  return <Container>Feito com carinho por Vinicius de Lima</Container>;
+};
--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -1,3 +1,4 @@
+import { Footer } from '../../components/Footer';
 import { Header } from '../../components/Header';
 import { MainContainer } from '../../components/MainContainer';
 import { PostData } from '../../domain/posts/post';
@@ -18,6 +19,7 @@ export default function HomePage({ posts }: HomePageProps) {
           ))}
         </Container>
       </MainContainer>
+      <Footer />
     </>
   );
 }
