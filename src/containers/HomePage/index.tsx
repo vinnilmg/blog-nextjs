ce838493104bb3d4e27ab4c11f66edfcc233847c
vinnilmg <vini.delimagomes@gmail.com>
2024-02-19 18:58:18 -0300

    componente Header

--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -1,3 +1,4 @@
+import { Header } from '../../components/Header';
 import { PostData } from '../../domain/posts/post';
 import { Container } from './styles';
 
@@ -7,10 +8,13 @@ export type HomePageProps = {
 
 export default function HomePage({ posts }: HomePageProps) {
   return (
-    <Container>
-      {posts.map((post) => (
-        <h2 key={post.attributes.slug}>{post.attributes.title}</h2>
-      ))}
-    </Container>
+    <>
+      <Header />
+      <Container>
+        {posts.map((post) => (
+          <h2 key={post.attributes.slug}>{post.attributes.title}</h2>
+        ))}
+      </Container>
+    </>
   );
 }
