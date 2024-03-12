42e971fb525128a7a8b37008a3ca95f8f21eac4b
vinnilmg <vini.delimagomes@gmail.com>
2024-03-11 21:01:12 -0300

    componente postCover

--- src/containers/Post/index.tsx
+++ src/containers/Post/index.tsx
@@ -2,6 +2,7 @@ import { Footer } from '../../components/Footer';
 import { Header } from '../../components/Header';
 import { Heading } from '../../components/Heading';
 import { MainContainer } from '../../components/MainContainer';
+import { PostCover } from '../../components/PostCover';
 import { PostData } from '../../domain/posts/post';
 import { concatenateContents } from '../../utils/concatenate-contents';
 
@@ -15,6 +16,10 @@ export const Post = ({ post }: PostProps) => {
       <Header />
       <MainContainer>
         <Heading>{post.attributes.title}</Heading>
+        <PostCover
+          coverUrl={post.attributes.cover.data.attributes.formats.large.url}
+          alt={post.attributes.title}
+        />
         <div
           dangerouslySetInnerHTML={{
             __html: concatenateContents(post.attributes.content),
