c999bb119a7d6c1a0ddfa12ec1c5b812cc9e6147
vinnilmg <vini.delimagomes@gmail.com>
2024-03-18 19:41:25 -0300

    fix: url

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -2,6 +2,6 @@ export const API_URL = 'https://blog-strapi-455r.onrender.com/api';
 export const POSTS_URL = `${API_URL}/posts`;
 
 export const SITE_NAME = 'Vinicius Gomes';
-export const SITE_URL = process.env.SITE_URL || 'http://127.0.0.1:3000';
+export const SITE_URL = 'https://blog-nextjs-course.netlify.app';
 
 export const DISQUS_BLOG_SHORTNAME = 'meublog-6oo8atsefg';
